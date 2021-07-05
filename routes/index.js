9ab99b3c5aec8fabd67f5f8f652142494f76f3db
Pedro <pkupersmit@gmail.com>
2021-07-04 23:22:54 -0300

    added routes comments

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,16 @@ const UserController = require("../controllers/userController");
 const UserService = require("../services/userSerivce");
 const UserInstance = new UserController(new UserService());
 
-//!Controllers
-
 /* GET home page. */
 router.get("/", function (req, res, next) {
   UserInstance.getUsers(req, res);
 });
 
+//Add new user
 router.post("/add", (req, res) => {
   UserInstance.addUser(req, res);
 });
-
+//Delete user
 router.delete("/delete/:id", (req, res) => {
   UserInstance.deleteUser(req, res);
 });
